f2ead8aac250951678d23ac1d22c8a93772d3c12
Rob Martin <rob.martin.5758@gmail.com>
2023-07-07 16:05:56 -0400

    update the blood drive

--- src/components/bloodDrive2023.js
+++ src/components/bloodDrive2023.js
@@ -1,16 +1,35 @@
 import React from "react"
 import * as styles from "./bloodDrive2023.module.css"
-
+import { StaticImage } from "gatsby-plugin-image"
 
 const BloodDrive2023 = () => {
   return (
-    <div id="blooddrive" className={styles.container} >
-      <div>
-        <iframe className={styles.frame}
-          src="https://mailchi.mp/31639742b872/your-help-is-needed-in-our-community-13817074"
-          // style={{ width: "100vw", height: "100vh" }}
-          title="Blood Drive"
-        ></iframe>
+    <div>
+      <div className={styles.container} id="bellTower">
+        <StaticImage
+          src="../../content/assets/bloodDrive2023.jpg"
+          alt="Blood Drive"
+          loading="eager"
+          object-fit="cover"
+          className={styles.image}
+        />
+      </div>
+      <div className={styles.footerContainer}>
+        <p className={styles.text}>Be sure to register ahead of time</p>
+        <p className={styles.text}>
+          <a href="https://www.redcrossblood.org/give.html/drive-results?zipSponsor=EpiphanyEden">
+            Register
+          </a>
+        </p>
+        <p className={styles.text}>
+          On July 19, fill out the Rapid Pass form to make your check-in faster
+          and easier
+        </p>
+        <p className={styles.text}>
+          <a href="https://www.redcrossblood.org/donate-blood/manage-my-donations/rapidpass.html">
+            Rapid Pass
+          </a>
+        </p>
       </div>
     </div>
   )
