462540f8e0d846c282e334ec51386ee190bcc241
Rob Martin <rob.martin.5758@gmail.com>
2023-07-07 14:48:21 -0400

    update bell tower treasure for this saturday

--- src/components/bellTowerTreasures.js
+++ src/components/bellTowerTreasures.js
@@ -3,14 +3,22 @@ import * as styles from "./bellTowerTreasures.module.css"
 import { StaticImage } from "gatsby-plugin-image"
 const BellTowerTreasures = () => {
   return (
-    <div className={styles.container} id="bellTower">
-      <StaticImage
-        src="../../content/assets/bellTowerTreasures.jpg"
-        alt="Bell Tower Treasures"
-        loading="eager"
-        object-fit="cover"
-        className={styles.image}
-      />
+    <div>
+      <div className={styles.headerContainer}>
+        <h1 className={styles.title}>Bell Tower Treasures</h1>
+        <h1 className={styles.title}>Coming Up:</h1>
+        <h1 className={styles.title}>Saturday July 8</h1>
+        <h1 className={styles.title}>9 am to 1 pm</h1>
+      </div>
+      <div className={styles.container} id="bellTower">
+        <StaticImage
+          src="../../content/assets/bellTowerTreasures.jpg"
+          alt="Bell Tower Treasures"
+          loading="eager"
+          object-fit="cover"
+          className={styles.image}
+        />
+      </div>
     </div>
   )
 }
