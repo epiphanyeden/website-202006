ebfa4f35bcd1375963ef717982edce0136a8954c
Rob Martin <rob.martin.5758@gmail.com>
2023-08-06 17:43:59 -0400

    bell tower updates

--- src/components/bellTowerTreasures.js
+++ src/components/bellTowerTreasures.js
@@ -4,12 +4,12 @@ import { StaticImage } from "gatsby-plugin-image"
 const BellTowerTreasures = () => {
   return (
     <div>
-      {/* <div className={styles.headerContainer}>
+      <div className={styles.headerContainer}>
         <h1 className={styles.title}>Bell Tower Treasures</h1>
         <h1 className={styles.title}>Coming Up:</h1>
-        <h1 className={styles.title}>Saturday July 8</h1>
+        <h1 className={styles.title}>Saturday August 12</h1>
         <h1 className={styles.title}>9 am to 1 pm</h1>
-      </div> */}
+      </div>
       <div className={styles.container} id="bellTower">
         <StaticImage
           src="../../content/assets/bellTowerTreasures.jpg"
