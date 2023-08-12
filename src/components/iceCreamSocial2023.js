7431d004d6a128b5adfe6be99c90e6562dbda03e
Rob Martin <rob.martin.5758@gmail.com>
2023-08-12 13:04:33 -0400

    update icream social

--- src/components/iceCreamSocial2023.js
+++ src/components/iceCreamSocial2023.js
@@ -4,94 +4,28 @@ import { StaticImage } from "gatsby-plugin-image"
 
 const IceCreamSocial2023 = () => {
   return (
-    <div>
-
-      <div className={styles.imageContainer} id="vbs">
+    <div className={styles.container}>
+      <div className={styles.imageContainer} id="icecream">
         <StaticImage
           src="../../content/assets/IceCreamSocial2023_large.jpg"
-          alt="Vaction Bible School"
+          alt="Ice Cream Social"
           loading="eager"
-          object-fit="cover"
           className={styles.image}
+          placeholder="blurred"
+          width={1000}
         />
       </div>
-      {/* <div className={styles.container}>
-        <h1 className={styles.title}>
-          Vacation Bible School 2023
+      <div className={styles.textContainer}>
+        <h1 className={styles.text}>
+          We are collecting school supplies for Douglass Elementary School.
         </h1>
-        <h1 className={styles.title}>
-          Live and In-Person
+        <h1 className={styles.text}>
+          A school supply list can be found on the&nbsp;<a href="https://www.douglasselementary.org/"> school website</a> &nbsp;under Quick Links.
         </h1>
-        <h1 className={styles.title}>
-          Hosted by Epiphany and Leaksville United Methodist
+        <h1 className={styles.text}>
+         We will also accept cash or&nbsp;<a href="https://tithe.ly/give_new/www/#/tithely/give-one-time/3897734"> credit card donations</a> &nbsp;for the school to use as needed.
         </h1>
-        <hr />
-        <a href="https://forms.gle/Brf2Hy9N4HcCdqVp7">
-          <button className={styles.button} >Register Here</button>
-        </a>
-        <hr />
-        <h1 className={styles.title}>
-          Sessions
-        </h1>
-        <h1 className={styles.title}>
-          Mon July 31 - Wed August 2
-        </h1>
-        <h1 className={styles.title}>
-          Check-in: 4:45 pm
-        </h1>
-        <h1 className={styles.title}>
-          Program: 5:15 - 7:30 pm
-        </h1>
-        <h1 className={styles.title}>
-          <a href=" https://goo.gl/maps/FbBcMDhgzSXU8hfH9">Leaksville UMC</a>
-        </h1>
-
-        <hr />
-        <h1 className={styles.title}>
-          All children age 3 through rising 6th Grade are invited to attend!
-        </h1>
-        <h1 className={styles.title}>(age 3 by July 1 and potty-trained)</h1>
-        <hr />
-        <h1 className={styles.title}>A light dinner will be served each night.</h1>
-        <hr />
-        <p className={styles.text}>
-          Hero Hotline
-        </p>
-        <p className={styles.text}>
-          Children will become Heroes!
-        </p>
-        <p className={styles.text}>
-          And discover:
-        </p>
-        <p className={styles.text}>
-          Everyone's gifts are valued!
-        </p>
-        <p className={styles.text}>
-          Every Hero has a role to play !
-        </p>
-        <hr />
-        <h1 className={styles.title}>
-          If you have any questions or can help with meals
-        </h1>
-        <h1 className={styles.title}>
-          Please email or call Deb Norton
-
-        </h1>
-        <h1 className={styles.title}>
-          <a href="mailto: epiphanyleaksvillevbs@gmail.com">epiphanyleaksvillevbs@gmail.com</a>
-         
-        </h1>
-        <h1 className={styles.title}>
-          <a href="tel:1-276-779-4006">276-779-4006</a>
-        </h1>
-        <figure className={styles.quote}>
-          <blockquote>
-            "So let’s strive together for the things that bring peace and the
-            things that build each other up."
-          </blockquote>
-          <figcaption>Romans 14:19</figcaption>
-        </figure>
-      </div> */}
+      </div>
     </div>
   )
 }
