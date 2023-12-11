bbb42e566a01da62028e19c7a48ec19684847637
Rob Martin <rob.martin.5758@gmail.com>
2023-12-11 16:48:49 -0500

    make it look nicer

--- src/components/advent2023.js
+++ src/components/advent2023.js
@@ -10,42 +10,52 @@ const Advent2023 = () => {
       <section className={styles.container}>
         <div className={styles.event}>
           <p className={styles.eventDate}>Dec 16</p>
-          <p className={styles.eventDay}>Sat</p>
+          <p className={styles.eventDay}>
+            Sat &nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;9:00 am to 3:00 pm
+          </p>
 
-          <p className={styles.eventTime}>9:00 am to 3:00 pm</p>
+          {/* <p className={styles.eventTime}>9:00 am to 3:00 pm</p> */}
 
           <p className={styles.eventDesc}>Advent Quiet Day</p>
         </div>
         <div className={styles.event}>
           <p className={styles.eventDate}>Dec 21</p>
 
-          <p className={styles.eventDay}>Thur</p>
-          <p className={styles.eventTime}>5:30 pm</p>
+          <p className={styles.eventDay}>Thur &nbsp;|&nbsp;&nbsp;5:30 pm</p>
+          {/* <p className={styles.eventTime}>5:30 pm</p> */}
 
           <p className={styles.eventDesc}>Longest Night Service</p>
         </div>
         <div className={styles.event}>
           <p className={styles.eventDate}>Dec 24</p>
-          <p className={styles.eventDay}>Sun</p>
-          <p className={styles.eventTime}>10:00 am</p>
+          <p className={styles.eventDay}>
+            Sun&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;10:00 am
+          </p>
+          {/* <p className={styles.eventTime}>10:00 am</p> */}
           <p className={styles.eventDesc}>Sunday Service</p>
         </div>
         <div className={styles.event}>
           <p className={styles.eventDate}>Dec 24</p>
-          <p className={styles.eventDay}>Sun</p>
-          <p className={styles.eventTime}>11:00 pm</p>
+          <p className={styles.eventDay}>
+            Sun&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;11:00 pm
+          </p>
+          {/* <p className={styles.eventTime}>11:00 pm</p> */}
           <p className={styles.eventDesc}>Christmas Eve Service</p>
         </div>
         <div className={styles.event}>
           <p className={styles.eventDate}>Dec 31</p>
-          <p className={styles.eventDay}>Sun</p>
-          <p className={styles.eventTime}>10:00 am</p>
+          <p className={styles.eventDay}>
+            Sun&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;10:00 am
+          </p>
+          {/* <p className={styles.eventTime}>10:00 am</p> */}
           <p className={styles.eventDesc}>Christmas Lessons & Carols</p>
         </div>
         <div className={styles.event}>
           <p className={styles.eventDate}>Jan 6</p>
-          <p className={styles.eventDay}>Sat</p>
-          <p className={styles.eventTime}>5:30 pm</p>
+          <p className={styles.eventDay}>
+            Sat&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;5:30 pm
+          </p>
+          {/* <p className={styles.eventTime}>5:30 pm</p> */}
           <p className={styles.eventDesc}>Annual Chili Cookoff</p>
         </div>
       </section>
