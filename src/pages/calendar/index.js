c341fcabd38d028574a4c63d3daa6df7e7048a83
Rob Martin <rob.martin.5758@gmail.com>
2024-02-15 11:31:15 -0500

    add calendar

---
+++ src/pages/calendar/index.js
@@ -0,0 +1,17 @@
+import React from "react"
+
+const CALENDAR = () => {
+  return (
+    <div>
+      <iframe
+        src="https://calendar.google.com/calendar/embed?height=600&wkst=1&ctz=America%2FNew_York&bgcolor=%23ffffff&src=ZXBpcGhhbnllZGVuQGdtYWlsLmNvbQ&src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&color=%234285F4&color=%23009688"
+        width="800"
+        height="600"
+        frameborder="0"
+        title="Calendar"
+      ></iframe>
+    </div>
+  )
+}
+
+export default CALENDAR
