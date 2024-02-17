8255d178abde2149f95e3d2c37d6a5714744af1c
Rob Martin <rob.martin.5758@gmail.com>
2024-02-17 18:24:49 -0500

    add taize service

--- src/components/taizeLent2024.js
+++ src/components/taizeLent2024.js
@@ -14,9 +14,11 @@ const TaizeLent2024 = () => {
       />
       <h1 className={styles.text}>
         A simple, meditative form of worship, calling us to dwell deeply on
-        Christ's presence around and within us. Mantra songs, prayerful silence,
-        and short readings guide the focus of the prayer in a candle-lit
-        environment.
+        Christ's presence around and within us.
+      </h1>
+      <h1 className={styles.text}>
+        Mantra songs, prayerful silence, and short readings guide the focus of
+        the prayer in a candle-lit environment.
       </h1>
       <h2 className={styles.smallText}>Wednesdays at 6 pm</h2>
     </div>
