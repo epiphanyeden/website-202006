b360e1e86ab17aabe9f4290a9758584fc02936e2
Rob Martin <rob.martin.5758@gmail.com>
2024-02-17 18:15:49 -0500

    add taize service

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,6 +14,7 @@ import Gift2023 from "./gift2023"
 import LitanyHolyLand from "./litanyHolyLand"
 import Events from "./events"
 import LentSunday2024 from "./lentStudy2024"
+import TaizeLent2024 from "./taizeLent2024"
 
 const Layout = ({ location, title, children }) => {
   return (
@@ -21,6 +22,7 @@ const Layout = ({ location, title, children }) => {
       <Header />
       <LandingPage />
       <Events />
+      <TaizeLent2024 />
       <LentSunday2024 />
       <LaundryLove />
       <BellTowerTreasures />
---
+++ src/components/taizeLent2024.js
@@ -0,0 +1,26 @@
+import React from "react"
+import * as styles from "./taizeLent2024.module.css"
+import "@fontsource/aboreto"
+import { StaticImage } from "gatsby-plugin-image"
+const TaizeLent2024 = () => {
+  return (
+    <div className={styles.container} id="taize">
+      <StaticImage
+        src="../../content/assets/taizeLent2023.jpg"
+        alt="Taize on Wednesdays 6 pm"
+        loading="lazy"
+        object-fit="cover"
+        className={styles.image}
+      />
+      <h1 className={styles.text}>
+        A simple, meditative form of worship, calling us to dwell deeply on
+        Christ's presence around and within us. Mantra songs, prayerful silence,
+        and short readings guide the focus of the prayer in a candle-lit
+        environment.
+      </h1>
+      <h2 className={styles.smallText}>Wednesdays at 6 pm</h2>
+    </div>
+  )
+}
+
+export default TaizeLent2024
