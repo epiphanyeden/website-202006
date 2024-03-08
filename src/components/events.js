5c5dc4f59c79718d447f6e3fb990c4fd9b6bddad
Rob Martin <rob.martin.5758@gmail.com>
2024-03-08 11:27:46 -0500

    Update events.js

--- src/components/events.js
+++ src/components/events.js
@@ -16,55 +16,55 @@ const Events = () => {
         </p>
       </section>
       <section className={styles.container}>
-        <div className={styles.event}>
+        {/* <div className={styles.event}>
           <p className={styles.eventDate}>Feb 21</p>
           <p className={styles.eventDay}>
             Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;11 am - Noon
           </p>
           <p className={styles.eventDesc}>Lenten Study @ St. Thomas</p>
-        </div>
-        <div className={styles.event}>
+        </div> */}
+        {/* <div className={styles.event}>
           <p className={styles.eventDate}>Feb 21</p>
           <p className={styles.eventDay}>
             Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 6 pm
           </p>
           <p className={styles.eventDesc}>Taize Service</p>
-        </div>
-        <div className={styles.event}>
+        </div> */}
+        {/* <div className={styles.event}>
           <p className={styles.eventDate}>Feb 28</p>
           <p className={styles.eventDay}>
             Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;11 am - Noon
           </p>
           <p className={styles.eventDesc}>Lenten Study @ St. Thomas</p>
-        </div>
-        <div className={styles.event}>
+        </div> */}
+        {/* <div className={styles.event}>
           <p className={styles.eventDate}>Feb 28</p>
           <p className={styles.eventDay}>
             Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 6 pm
           </p>
           <p className={styles.eventDesc}>Taize Service</p>
-        </div>
-        <div className={styles.event}>
+        </div> */}
+        {/* <div className={styles.event}>
           <p className={styles.eventDate}>March 2</p>
           <p className={styles.eventDay}>
             Sat&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 9 am - 11 am
           </p>
           <p className={styles.eventDesc}>Laundry Love</p>
-        </div>
-        <div className={styles.event}>
+        </div> */}
+        {/* <div className={styles.event}>
           <p className={styles.eventDate}>Mar 6</p>
           <p className={styles.eventDay}>
             Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;11 am - Noon
           </p>
           <p className={styles.eventDesc}>Lenten Study @ Epiphany</p>
-        </div>
-        <div className={styles.event}>
+        </div> */}
+        {/* <div className={styles.event}>
           <p className={styles.eventDate}>Mar 6</p>
           <p className={styles.eventDay}>
             Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 6 pm
           </p>
           <p className={styles.eventDesc}>Taize Service</p>
-        </div>
+        </div> */}
         <div className={styles.event}>
           <p className={styles.eventDate}>March 9</p>
           <p className={styles.eventDay}>
@@ -73,11 +73,101 @@ const Events = () => {
           <p className={styles.eventDesc}>Bell Tower Treasures</p>
         </div>
         <div className={styles.event}>
-          <p className={styles.eventDate}>Mar 6</p>
+          <p className={styles.eventDate}>Mar 13</p>
           <p className={styles.eventDay}>
-            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; TBD
+            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;11 am - Noon
+          </p>
+          <p className={styles.eventDesc}>
+            Coffee and Conversation @ St. Luke's
+          </p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 13</p>
+          <p className={styles.eventDay}>
+            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 6 pm
           </p>
           <p className={styles.eventDesc}>Taize Service</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 20</p>
+          <p className={styles.eventDay}>
+            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp;11 am - Noon
+          </p>
+          <p className={styles.eventDesc}>Coffee and Conversation @ Epiphany</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 20</p>
+          <p className={styles.eventDay}>
+            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 6 pm
+          </p>
+          <p className={styles.eventDesc}>Taize Service</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 21</p>
+          <p className={styles.eventDay}>
+            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 5:30 pm
+          </p>
+          <p className={styles.eventDesc}>
+            Solace Seekers (Grief Support Group)
+          </p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 24</p>
+          <p className={styles.eventDay}>
+            Sun&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 10 am
+          </p>
+          <p className={styles.eventDesc}>Palm Sunday Service</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 27</p>
+          <p className={styles.eventDay}>
+            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 6 pm
+          </p>
+          <p className={styles.eventDesc}>Taize Service</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 28</p>
+          <p className={styles.eventDay}>
+            Thu&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 7 pm
+          </p>
+          <p className={styles.eventDesc}>
+            Maundy Thursday Communion and Foot Washing
+          </p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 29</p>
+          <p className={styles.eventDay}>
+            Fri&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; Noon
+          </p>
+          <p className={styles.eventDesc}>Good Friday Service</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 29</p>
+          <p className={styles.eventDay}>
+            Fri&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 5:30 pm
+          </p>
+          <p className={styles.eventDesc}>Stations of the Cross</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 29</p>
+          <p className={styles.eventDay}>
+            Fri&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 7 pm
+          </p>
+          <p className={styles.eventDesc}>Tenebrae @ LUMC</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 30</p>
+          <p className={styles.eventDay}>
+            Sat&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 8 pm
+          </p>
+          <p className={styles.eventDesc}>Easter Vigil Service</p>
+        </div>
+        <div className={styles.event}>
+          <p className={styles.eventDate}>Mar 31</p>
+          <p className={styles.eventDay}>
+            Sun&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 10 am
+          </p>
+          <p className={styles.eventDesc}>Easter Sunday Service</p>
         </div>
         {/* <div className={styles.event}>
           <p className={styles.eventDate}>Mar 13</p>
