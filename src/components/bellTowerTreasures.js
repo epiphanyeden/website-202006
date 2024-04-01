ef0247925fa8bdc4e85fb2bedaa5a280eb5ce691
Rob Martin <rob.martin.5758@gmail.com>
2024-04-01 17:48:45 -0400

    Update BTT

--- src/components/bellTowerTreasures.js
+++ src/components/bellTowerTreasures.js
@@ -4,17 +4,7 @@ import { StaticImage } from "gatsby-plugin-image"
 const BellTowerTreasures = () => {
   return (
     <div className={styles.outerContainer} id="btt">
-      <div className={styles.textContainer}>
-        <h1 className={styles.title}>Bell Tower Treasures</h1>
-        <h1 className={styles.title}>No Sale In April</h1>
-        <h1 className={styles.title}>Coming Up:</h1>
-        <h1 className={styles.title}>Saturday May 11</h1>
-        <h1 className={styles.title}>9 am to 1 pm</h1>
-        <h1 className={styles.text}>
-          Drop off donations on Tuesdays, 9:30 am until noon
-        </h1>
-      </div>
-      <div className={styles.imageContainer}>
+      {/* <div className={styles.imageContainer}>
         <StaticImage
           src="../../content/assets/Bell Tower Treasures.jpg"
           alt="Bell Tower Treasures"
@@ -22,6 +12,31 @@ const BellTowerTreasures = () => {
           object-fit="cover"
           className={styles.image}
         />
+      </div> */}
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Bell Tower Treasures</h1>
+        <p className={styles.text}>Next Sale:</p>
+        <p className={styles.text}>Saturday May 11, 9 am to 1 pm</p>
+        {/* <p className={styles.text}>9 am to 1 pm</p> */}
+      </div>
+      <div className={styles.textContainer}>
+        <p className={styles.smallText}>
+          In order to streamline our process and organize inventory, BellTower
+          Treasures will NOT be open for sales in April.
+        </p>
+        <p className={styles.smallText}>
+          However, please join us to shop our newly updated merchandise, on
+          Saturday, May 11, from 9am to 1pm.
+        </p>
+        <p className={styles.smallText}>
+          Donations will continue to be accepted through April, as always:
+          Tuesdays from 9am to 2:30pm or appointment, by calling 336-623-9410.
+        </p>
+        <p className={styles.smallText}>
+          We wish to thank all our customers for their continued support of Bell
+          Tower Treasures. Remember proceeds go to support Outreach Programs at
+          Church of the Epiphany!
+        </p>
       </div>
     </div>
   )
