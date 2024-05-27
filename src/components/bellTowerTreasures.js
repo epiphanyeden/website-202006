6d2ea23199cc2ab1621aaa27861d5e6d26274ca9
Rob Martin <rob.martin.5758@gmail.com>
2024-05-27 10:00:21 -0400

    update BTT

--- src/components/bellTowerTreasures.js
+++ src/components/bellTowerTreasures.js
@@ -16,18 +16,10 @@ const BellTowerTreasures = () => {
       <div className={styles.textContainer}>
         <h1 className={styles.title}>Bell Tower Treasures</h1>
         <p className={styles.text}>Next Sale:</p>
-        <p className={styles.text}>Saturday June 8, 9 am to 1 pm</p>
-        {/* <p className={styles.text}>9 am to 1 pm</p> */}
+        <p className={styles.text}>Saturday June 8</p>
+        <p className={styles.text}>9 am to 1 pm</p>
       </div>
       <div className={styles.textContainer}>
-        <p className={styles.smallText}>
-          In order to streamline our process and organize inventory, Bell Tower
-          Treasures will NOT be open for sales in April.
-        </p>
-        <p className={styles.smallText}>
-          However, please join us to shop our newly updated merchandise, on
-          Saturday, May 11, from 9 am to 1 pm.
-        </p>
         <p className={styles.smallText}>
           Donations will continue to be accepted on Tuesdays from 9 am to 2:30
           pm or by calling 336-623-9410 for an appointment.
