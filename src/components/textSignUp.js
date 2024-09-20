dc211fb4ff041f08296ed1ceb4e2003fe2895d34
Rob Martin <rob.martin.5758@gmail.com>
2024-09-20 12:16:58 -0400

    fix textmagic signup form

--- src/components/textSignUp.js
+++ src/components/textSignUp.js
@@ -1,26 +1,71 @@
-import React from "react"
-import * as styles from "./textSignUp.module.css"
+// import React from "react"
+// import * as styles from "./textSignUp.module.css"
 
-const textSignUp = () => {
+// ;<script
+//   id="textmagic_widget_id"
+//   src="https://widgets.textmagic.com/widget-script.js"
+//   data-code="657e81770d34809"
+//   data-type="subscribe"
+//   defer
+// ></script>
+
+// const textSignUp = () => {
+//   return (
+//     <div className={styles.container} id="textSignUp">
+//       <div className="tm-widget">
+//         <div className="tm-widget-inner">
+//           <iframe
+//             src="https://my.textmagic.com/userwidgets/cmd/show/code/657e81770d34809/default"
+//             width="568"
+//             height="349"
+//             frameborder="0"
+//             marginheight="0"
+//             marginwidth="0"
+//             title="Text SignUp"
+//           >
+//             Loading...
+//           </iframe>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default textSignUp
+
+import React, { useEffect } from "react"
+import { Helmet } from "react-helmet"
+
+const TextSignUp = () => {
+  useEffect(() => {
+    // This effect will run after the component mounts
+    const script = document.createElement("script")
+    script.src = "https://widgets.textmagic.com/widget-script.js"
+    // script.id = "textSignUp"
+    script.id = "textmagic_widget_id"
+    script.setAttribute("data-code", "a56b070e3862602")
+    script.setAttribute("data-type", "subscribe")
+    script.defer = false
+    document.body.appendChild(script)
+
+    // Cleanup function to remove the script when the component unmounts
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, []) // Empty dependency array means this effect runs once on mount
+
   return (
-    <div className={styles.container} id="textSignUp">
-      <div className="tm-widget">
-        <div className="tm-widget-inner">
-          <iframe
-            src="https://my.textmagic.com/userwidgets/cmd/show/code/657e81770d34809/default"
-            width="568"
-            height="349"
-            frameborder="0"
-            marginheight="0"
-            marginwidth="0"
-            title="Text SignUp"
-          >
-            Loading...
-          </iframe>
+    <>
+      {/* <Helmet> */}
+      {/* You can add any additional head elements here if needed */}
+      {/* </Helmet> */}
+      <div id="textSignUp">
+        <div id="textmagic-form-container">
+          {/* The TextMagic form will be injected here by their script */}
         </div>
       </div>
-    </div>
+    </>
   )
 }
 
-export default textSignUp
+export default TextSignUp
