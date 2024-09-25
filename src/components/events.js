15bbf8bc152dc2ae65b1d698470738fc7f4c5eb2
Rob Martin <rob.martin.5758@gmail.com>
2024-09-25 17:35:51 -0400

    renove comments

--- src/components/events.js
+++ src/components/events.js
@@ -16,15 +16,6 @@ const Events = () => {
         </p>
       </section>
       <section className={styles.container}>
-        {/* <div className={`${styles.event} ${styles.juneteenth}`}>
-          <p className={styles.eventDate}>June 19</p>
-          <p className={styles.eventDesc}>Juneteenth</p>
-        </div> */}
-        {/* <div className={`${styles.event} ${styles.july4}`}>
-          <p className={styles.eventDate}>July 4</p>
-          <p className={styles.eventDesc}>Independence Day</p>
-        </div> */}
-
         <div className={styles.event}>
           <p className={styles.eventDate}>Sept 27</p>
           <p className={styles.eventDay}>
@@ -53,55 +44,6 @@ const Events = () => {
           </p>
           <p className={styles.eventDesc}>Bell Tower Treasures</p>
         </div>
-        {/* <div className={styles.event}>
-          <p className={styles.eventDate}>Mar 13</p>
-          <p className={styles.eventDay}>
-            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; TBD
-          </p>
-          <p className={styles.eventDesc}>Taize Service</p>
-        </div> */}
-        {/* <div className={styles.event}>
-          <p className={styles.eventDate}>Mar 20</p>
-          <p className={styles.eventDay}>
-            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; TBD
-          </p>
-          <p className={styles.eventDesc}>Taize Service</p>
-        </div> */}
-        {/* <div className={styles.event}>
-          <p className={styles.eventDate}>Mar 27</p>
-          <p className={styles.eventDay}>
-            Wed&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; TBD
-          </p>
-          <p className={styles.eventDesc}>Taize Service</p>
-        </div> */}
-        {/* <div className={styles.event}>
-          <p className={styles.eventDate}>Mar 28</p>
-          <p className={styles.eventDay}>
-            Thur&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; TBD
-          </p>
-          <p className={styles.eventDesc}>Maundy Thursday Service</p>
-        </div> */}
-        {/* <div className={styles.event}>
-          <p className={styles.eventDate}>Mar 29</p>
-          <p className={styles.eventDay}>
-            Fri&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; TBD
-          </p>
-          <p className={styles.eventDesc}>Good Friday Service</p>
-        </div> */}
-        {/* <div className={styles.event}>
-          <p className={styles.eventDate}>Mar 31</p>
-          <p className={styles.eventDay}>
-            Sun&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 9:30 am
-          </p>
-          <p className={styles.eventDesc}>Flowering of the Cross</p>
-        </div> */}
-        {/* <div className={styles.event}>
-          <p className={styles.eventDate}>Mar 31</p>
-          <p className={styles.eventDay}>
-            Sun&nbsp;&nbsp;&nbsp;&nbsp;| &nbsp;&nbsp; 10 am
-          </p>
-          <p className={styles.eventDesc}>Easter Sunday Service</p>
-        </div> */}
       </section>
     </div>
   )
