45bba8118a66bd0e2f2d0a65a46eb497c95fa211
Rob Martin <rob.martin.5758@gmail.com>
2024-10-09 13:44:48 -0400

    make it look better, maybe

--- src/components/bishopVisit2024.js
+++ src/components/bishopVisit2024.js
@@ -14,37 +14,43 @@ const BishopVisit2024 = () => {
           placeholder="blurred"
           width={500}
         />
-        <figcaption>
+        <figcaption className={styles.caption}>
           Courtesy of Episcopal Diocese of North Carolina.
         </figcaption>
       </div>
-      <div className={styles.textWrapper}>
-        <h1 className={styles.heading}>Bishop Jennifer Visiting</h1>
-        <h1 className={styles.heading}>Sunday October 20</h1>
-        <p>
-          Church of the Epiphany and St. Luke's will welcome{" "}
-          <a href="https://www.episdionc.org/bishops/">
-            The Rt. Rev. Jennifer Brooke-Davidson
-          </a>{" "}
-          , Assistant Bishop of NC, on October 20 at Church of the Epiphany.
-        </p>
-        <p>
-          Please plan on being at Epiphany by 9 am for a teaching opportunity
-          with Bishop Jennifer.
-        </p>
-        <p>The service will be at the normal time of 10 am.</p>
-        <p>
-          There will be a reception for Bishop Jennifer following the service.
-        </p>
-        <p>
-          Please note: Your contribution to the Bishop's Discretionary Fund,
-          based on the loose plate offering, is very much appreciated during
-          this time. Your offering helps us to care for and reach out to those
-          who are vulnerable and in need. Another way to give is using the{" "}
-          <a href="https://givingtools.com/give/1182/1995">
-            online giving link.
-          </a>
-        </p>
+      <div className={styles.descWrapper}>
+        <div className={styles.headingWrapper}>
+          <h1 className={styles.heading}>Bishop Jennifer Visit</h1>
+          <h1 className={styles.heading}>Sunday October 20</h1>
+        </div>
+        <div className={styles.textWrapper}>
+          <p className={styles.text}>
+            Church of the Epiphany and St. Luke's will welcome{" "}
+            <a href="https://www.episdionc.org/bishops/">
+              The Rt. Rev. Jennifer Brooke-Davidson
+            </a>{" "}
+            , Assistant Bishop of NC, on October 20 at Church of the Epiphany.
+          </p>
+          <p className={styles.text}>
+            Please plan on being at Epiphany by 9 am for a teaching opportunity
+            with Bishop Jennifer.
+          </p>
+          <p className={styles.text}>
+            The service will be at the normal time of 10 am.
+          </p>
+          <p className={styles.text}>
+            There will be a reception for Bishop Jennifer following the service.
+          </p>
+          <p className={styles.text}>
+            Please note: Your contribution to the Bishop's Discretionary Fund,
+            based on the loose plate offering, is very much appreciated during
+            this time. Your offering helps us to care for and reach out to those
+            who are vulnerable and in need. Another way to give is using the{" "}
+            <a href="https://givingtools.com/give/1182/1995">
+              online giving link.
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   )
