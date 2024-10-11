55621049e075e91a1aca87344876ba4848848602
Rob Martin <rob.martin.5758@gmail.com>
2024-10-11 11:30:15 -0400

    update pledge form and email

--- env/production.js
+++ env/production.js
@@ -1,6 +1,6 @@
 module.exports = {
-  EMAILJS_SENDEREMAIL: "gift@epiphanyeden.org",
+  EMAILJS_SENDEREMAIL: "tech@epiphanyeden.org",
   EMAILJS_TEMPLATEID: "template_1py5tib",
   EMAILJS_USER: "0aFoe9UtGBfwhab5D",
-  EMAILJS_SERVICE: "service_x7uqaqq",
-  }
+  EMAILJS_SERVICE: "service_l3e69eh",
+}
