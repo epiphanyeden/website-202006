887e85b4caf3e1f9b7bc9f05038511c6a05facf8
Rob Martin <rob.martin.5758@gmail.com>
2025-10-17 16:49:45 -0400

    remove boa add trunk or treat

--- src/components/layout.js
+++ src/components/layout.js
@@ -14,7 +14,7 @@ import Gift2024 from "./gift2024"
 import Events from "./events"
 import WhatIsEpiscopal from "./whatIsEpiscopal"
 import ChildrenChurch from "./childrenChurch"
-import BOA2025 from "./boa2025"
+import TrunkTreat2025 from "./trunkTreat2025"
 
 const Layout = ({ location, title, children }) => {
   return (
@@ -23,10 +23,10 @@ const Layout = ({ location, title, children }) => {
       <LandingPage />
       <Events />
       <WhatIsEpiscopal />
-      <BellTowerTreasures />
-      <BOA2025 />
-      <LaundryLove />
       <ChildrenChurch />
+      <TrunkTreat2025 />
+      <LaundryLove />
+      <BellTowerTreasures />
       <Gift2024 />
       <ChurchLinks />
       <Announcements />
